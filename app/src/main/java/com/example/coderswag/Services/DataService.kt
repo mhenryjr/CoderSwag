095f00af6b6cacc914e67991ab88204257bfbd1f
Your Name <you@example.com>
2020-01-12 11:23:09 -0800

    lesson-02

---
+++ app/src/main/java/com/example/coderswag/Services/DataService.kt
@@ -0,0 +1,38 @@
+package com.example.coderswag.Services
+
+import android.icu.util.ULocale
+import com.example.coderswag.Model.Category
+import com.example.coderswag.Model.Product
+import java.util.*
+
+object DataService {
+
+    val catagories = listOf(
+        Category("SHIRTS", "shirtimage"),
+        Category("HOODIES", "hoodieimage"),
+        Category("HATS", "hatsimage"),
+        Category("DIGITAL", "digitalgoodsimage")
+    )
+
+    val hats = listOf(
+        Product("Devslopes Graphic Beanie", "$18", "hat01"),
+        Product("Devslopes Hat Black", "$20", "hat02"),
+        Product("Devslopes Hat White", "$18", "hat03"),
+        Product("Devslopes Snapback", "$22", "hat04")
+    )
+
+    val hoodies = listOf(
+        Product("Devslopes Hoodie Grey", "$28", "hoodie01"),
+        Product("Devslopes Hoodie Red", "$32", "hoodie02"),
+        Product("Devslopes Grey Hoodie", "$28", "hoodie03"),
+        Product("Devslopes Black Hoodie", "$32", "hoodie04")
+    )
+
+    val shirt = listOf(
+        Product("Devslopes Shirt Black", "$18", "shirt01"),
+        Product("Devslopes Badge Light Grey", "$20", "shirt02"),
+        Product("Devslopes Logo Shirt Red", "$22", "shirt03"),
+        Product("Devslopes Hustle", "$22", "shirt04"),
+        Product("Kickflip Studios", "$18", "shirt05")
+    )
+}
